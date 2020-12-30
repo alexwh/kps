d12308475d79ea33eaea473ac7dcb3c7b3c06f13
alexwh <alexwh@users.noreply.github.com>
2020-12-30 21:55:54 +0000

    models + migrations

--- charts/models.py
+++ charts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class CommentAuthor(models.Model):
+    userid = models.CharField(max_length=24, primary_key=True, unique=True)
+    avatar = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+
+class Stream(models.Model):
+    stream_id = models.CharField(max_length=11, primary_key=True, unique=True)
+    title = models.CharField(max_length=100)
+    thumbnail = models.URLField()
+    description = models.TextField()
+
+class StreamComment(models.Model):
+    message_id = models.CharField(max_length=100, primary_key=True, unique=True)  # 100ish, should be 82 + 3 for potential doublepad
+    message = models.CharField(max_length=200)
+    message_type = models.CharField(max_length=50)
+
+    stream = models.ForeignKey(Stream, on_delete=models.CASCADE)
+    author = models.ForeignKey(CommentAuthor, on_delete=models.CASCADE)
+
+    timestamp = models.DateTimeField()
+    action_type = models.CharField(max_length=50)
+    relative_time = models.DurationField()
