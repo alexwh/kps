c3ce17e1f00c46b108c87ea275a83f8fa4012af1
alexwh <alexwh@users.noreply.github.com>
2020-12-30 21:54:51 +0000

    update template

--- charts/urls.py
+++ charts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path('', views.index.as_view(), name='index'),
-    path('_test', views.test, name='test'),
+    path('', TemplateView.as_view(template_name='line_chart.html'), name='line_chart'),
+    path('cinfo/<ytid>.json', views.LineChartJSONView.as_view(), name='line_chart_json'),
 ]
