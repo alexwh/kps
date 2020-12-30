b0a54297c5e9ea91cc01dd190b76fe3bd21fe7c3
alexwh <alexwh@users.noreply.github.com>
2020-12-30 23:02:16 +0000

    add Channel foreign key to Stream

--- charts/models.py
+++ charts/models.py
@@ -10,6 +10,7 @@ class Stream(models.Model):
     title = models.CharField(max_length=100)
     thumbnail = models.URLField()
     description = models.TextField()
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
 
 class StreamComment(models.Model):
     message_id = models.CharField(max_length=100, primary_key=True, unique=True)  # 100ish, should be 82 + 3 for potential doublepad
