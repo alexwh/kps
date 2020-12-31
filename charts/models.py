31fd03a47d359cf14a6a1b1ddbcd785e9efde206
alexwh <alexwh@users.noreply.github.com>
2020-12-31 19:15:18 +0000

    rename userid -> user_id

--- charts/models.py
+++ charts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Channel(models.Model):
-    userid = models.CharField(max_length=24, primary_key=True, unique=True)
+    user_id = models.CharField(max_length=24, primary_key=True, unique=True)
     avatar = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
 
@@ -14,8 +14,8 @@ class Stream(models.Model):
 
 class StreamComment(models.Model):
     message_id = models.CharField(max_length=100, primary_key=True, unique=True)  # 100ish, should be 82 + 3 for potential doublepad
-    message = models.CharField(max_length=200)
     message_type = models.CharField(max_length=50)
+    message = models.CharField(max_length=200)
 
     stream = models.ForeignKey(Stream, on_delete=models.CASCADE, related_name="comments")
     channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
