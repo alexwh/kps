e36850c79e6c13c2cb873c1a6088e406b56d4a63
alexwh <alexwh@users.noreply.github.com>
2020-12-31 20:13:44 +0000

    preliminary ws session stuff

--- charts/consumers.py
+++ charts/consumers.py
@@ -1,24 +1,40 @@
+from uuid import uuid4
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django_q.tasks import async_task
+from asgiref.sync import sync_to_async
 
 class FetchConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        await self.accept()
-        # self.user = self.scope['user']
-        # self.session = self.scope['session']
-        # if self.user.is_authenticated:
-        #     await self.accept()
-        # else:
-        #     await self.reject()
+        self.ytid = self.scope["url_route"]["kwargs"]["ytid"]
+        self.user = self.scope["user"]
+        self.session = self.scope["session"]
+        if self.user.is_authenticated:
+            ws_user_id = str(self.user.id)
+        else:
+            ws_user_id = str(self.user) + str(uuid4())
 
-    async def fetch_comments(request):
-        async_task('tasks.fetch_comments_ytid',
+        self.session["ws_user_id"] = ws_user_id
+        self.group_name = ws_user_id
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name)
+
+        await sync_to_async(self.session.save)()
+        await self.accept()
+
+    async def fetch_comments(self, request):
+        async_task("tasks.fetch_comments_ytid",
                    request.user,
                    request.ytid,
-                   hook='tasks.websocket_report')
+                   hook=self.websocket_report)
 
-    async def receive(self, ytid):
-        await self.send(text_data="Hello world!")
+    async def websocket_report(self, task):
+        if task.success:
+            self.send("fetched", task.result)
+        else:
+            self.send("did not fetch", task.result)
 
-    async def disconnect(self, close_code):
-        pass
+    async def receive(self, text_data=None):
+        self.session["timestamp"] = 10000
+        await sync_to_async(self.session.save)()
+        await self.send("fetch_for " + str(self.session.items()))
