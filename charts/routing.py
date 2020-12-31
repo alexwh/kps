bb57ef4d5a70a34350f84315b929ded24bbca3dd
alexwh <alexwh@users.noreply.github.com>
2020-12-31 23:40:13 +0000

    accurate youtube id regex for websocket route

--- charts/routing.py
+++ charts/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/fetch/(?P<ytid>\w+)/$', consumers.FetchConsumer.as_asgi(), name="fetch_consumer"),
+    re_path(r'ws/fetch/(?P<ytid>[0-9A-Za-z_-]{11})/$', consumers.FetchConsumer.as_asgi(), name="fetch_consumer"),
 ]
