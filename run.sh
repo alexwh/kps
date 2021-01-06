22467fa481dc5ee926519be730f0adbbd6a9b3be
alexwh <alexwh@users.noreply.github.com>
2021-01-06 05:55:08 +0000

    run on 0.0.0.0

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 pipenv run python manage.py migrate
 (pipenv run python manage.py qcluster &
-pipenv run python manage.py runserver)
+pipenv run python manage.py runserver 0.0.0.0:8000)
